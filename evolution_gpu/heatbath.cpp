3a43b6666877accd39346fe6fa444341347ad85a
Louis Primeau <louis.primeau@mail.utoronto.ca>
2022-09-13 15:18:11 -0400

    starting cuda development

---
+++ evolution_gpu/heatbath.cpp
@@ -0,0 +1,41 @@
+#include <torch/extension.h>
+#include <iostream>
+#include <vector>
+
+torch::Tensor partition_function(torch::Tensor v, torch::Tensor s) {
+  auto e = torch::exp(torch::addmm(s * v));
+  return e;
+}
+
+torch::Tensor heatbath(torch::Tensor field, torch::Tensor T, torch::Tensor ) {
+    
+}
+
+std::vector<at::Tensor> lltm_forward(
+    torch::Tensor input,
+    torch::Tensor weights,
+    torch::Tensor bias,
+    torch::Tensor old_h,
+    torch::Tensor old_cell) {
+  auto X = torch::cat({old_h, input}, /*dim=*/1);
+
+  auto gate_weights = torch::addmm(bias, X, weights.transpose(0, 1));
+  auto gates = gate_weights.chunk(3, /*dim=*/1);
+
+  auto input_gate = torch::sigmoid(gates[0]);
+  auto output_gate = torch::sigmoid(gates[1]);
+  auto candidate_cell = torch::elu(gates[2], /*alpha=*/1.0);
+
+  auto new_cell = old_cell + candidate_cell * input_gate;
+  auto new_h = torch::tanh(new_cell) * output_gate;
+
+  return {new_h,
+          new_cell,
+          input_gate,
+          output_gate,
+          candidate_cell,
+          X,
+          gate_weights};
+}
+
+
